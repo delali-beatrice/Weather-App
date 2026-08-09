8e43456327d88fb795fcba576d68d1e9787ef66a
Kpemini Delali Beatrice <kpeminibeatrice18@gmail.com>
2026-08-09 01:11:05 +0000

    modified files

--- weather_app/scripts/location.js
+++
@@ -1,8 +0,0 @@
-export function getCurrentCoords() {
-    return new Promise ((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => resolve(position.coords),
-            (error) => reject(error)
-        );
-    });
-}
